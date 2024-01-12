424a67c19485b3d4bff9a8a84a4aede07b046868
salvadorcar <143309978+salvadorcar@users.noreply.github.com>
2024-01-12 17:46:59 -0600

    Add files via upload

--- chat.js
+++ chat.js
@@ -2,6 +2,10 @@
 //AGREGA TUS ENLACES DE FIREBASE AQUÍ
 
 
+var firebaseConfig ={
+    apiKe: "AIzaSyAk_gg7lHoMJdVli="
+}
+
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -10,8 +14,19 @@ function addUser()
 {
     user_name = document.getElementById("user_name").value;
     firebase.database().ref("/").child(user_name).update({
-        purpose : "adding user"
-    });
+       
+        var firebaseConfig= {
+
+        apiKey: "AIzaSyAK_gg7LHoMoJdVlisKbiUdJ35idJDg-P9s",
+        authDomain : "social-e5855.firebaseapp.com",
+        datavasseURL : "https://social-e5855-default-rtdb.firebaseio.com",
+        proyectId : "social-e5855"
+        storageBuket : "social-e5855.appspot.com",
+        messagingSenderId: "671543009162",
+        appId: "1:671543009162:web:a881fdddf02bf05c08b273",
+
+
+}});
     
     localStorage.setItem("user_name", user_name);
 
